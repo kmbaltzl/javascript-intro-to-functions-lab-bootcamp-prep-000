0e1100f68cc53d4cad93e050fe598f318cd350c4
Katie Baltzley <kmbaltzl@gmail.com>
2018-07-30 14:13:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  var string = "hello"
+ if()
+ var string = "hello"
   string.toLowerCase() === string
   return "I can\'t hear you!"
 }
